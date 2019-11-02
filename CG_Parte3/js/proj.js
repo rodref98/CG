0e75714b5a8979bc2a1da3b524a8ee60d4ae9d62
Aylton Silva <38221744+ayltonsilva@users.noreply.github.com>
2019-11-02 14:51:33 +0000

    Holofotes almost done

--- CG_Parte3/js/proj.js
+++ CG_Parte3/js/proj.js
@@ -35,6 +35,18 @@ class Wall extends Base_Object {
   }
 }
 
+class Holofote extends Base_Object {
+  constructor(x, y, z){
+    super();
+    createHolofote(this, x, y, z);
+  }
+
+  myType(){
+    return "Holofote";
+  }
+
+}
+
 class Triangle extends Base_Object {
     constructor(x, y, z){
       super();
@@ -97,7 +109,7 @@ function createWall(table, x, y, z) {
     material = new THREE.MeshBasicMaterial({ color: 0x7FFFD4, wireframe: wires });
     addGroundWall(table, 0, 0, 0);
     addBackWall(table, 0, 20, -29);
-    addPedestal(table, 0, 11, 0);
+    addPedestal(table, 0, 10, 0);
 
     table.position.x = x;
     table.position.y = y;
@@ -105,6 +117,31 @@ function createWall(table, x, y, z) {
     grupo.add(table);
 }
 
+function addHolofoteCone(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.CylinderGeometry(4, 0, 10, 22, 0, true);
+    mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(x, y, z);
+
+    obj.add(mesh);
+}
+
+function createHolofote(index, x, y, z) {
+    'use strict';
+
+
+    material = new THREE.MeshBasicMaterial({ color: 0x1E90FF, wireframe: wires });
+    addHolofoteCone(index, 0, 10, 0);
+
+    index.rotation.z = Math.PI/2;
+    index.rotation.y = -Math.PI/2;
+    index.position.x = x;
+    index.position.y = y;
+    index.position.z = z;
+    grupo.add(index);
+}
 
 
 
@@ -117,6 +154,10 @@ function createScene() {
 
     new Triangle(5,5,5);
     new Wall(0,0,0);
+    new Holofote(-25, 25, 30);
+    new Holofote(-10, 25, 30);
+    new Holofote(5, 25, 30);
+    new Holofote(20, 25, 30);
 
     scene.add(grupo);
 
@@ -142,7 +183,7 @@ function createCamera2() {
   camera1[1] = new THREE.PerspectiveCamera( 45, 1920/1080, 1, 1000);
 
 
-  camera1[1].position.x =0;
+  camera1[1].position.x = 40;
   camera1[1].position.y = 40;
   camera1[1].position.z = 100;
   camera1[1].lookAt(scene.position);
@@ -186,8 +227,6 @@ function onKeyDown(e) {
     case 50: //2
         switch_camera(1);
         break;
-
-    case 51: //3
     case 37://left arrow
         selected_cannon.toggleLeftMovement();
         break
