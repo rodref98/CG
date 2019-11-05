53578cd7ada8bd6900960eb51f94131bf277e2e4
Aylton Silva <38221744+ayltonsilva@users.noreply.github.com>
2019-11-05 18:52:18 +0000

    Global light done

--- CG_Parte3/js/proj.js
+++ CG_Parte3/js/proj.js
@@ -9,18 +9,23 @@ var wires = true;
 var geometry, material, mesh;
 var grupo = new THREE.Group();
 var table;
-
+var directional_light;
+var holofote2;
+var material_array;
+var material_counter = 0;
 
 class Base_Object extends THREE.Object3D{
   constructor(){
     super();
 	}
 
+  changeMaterial(material){
+    this.children[0].material = material;
+  }
+
   myType(){
     return "Object";
   }
-
-
 
 }
 
@@ -89,7 +94,7 @@ function createTriangle(obj,x,y,z){
 
 function addGroundWall(obj, x, y, z) {
   'use strict';
-  geometry = new THREE.CubeGeometry(80, 0, 80);
+  geometry = new THREE.CubeGeometry(80, 2, 80);
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(x, y, z);
   obj.add(mesh);
@@ -119,14 +124,14 @@ function addPedestalTop(obj,x,y,z){
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
     obj.add(mesh);
-   
+
 }
 
 function createWall(table, x, y, z) {
     'use strict';
 
 
-    material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: wires });
+    material = new THREE.MeshBasicMaterial({ color: 0x667C26, wireframe: wires });
     addGroundWall(table, 0, 0, 40);
     addBackWall(table, 0, 30, 1);
     addPedestalLeg(table, 0, 10, 45);
@@ -163,6 +168,7 @@ function createHolofote(index, x, y, z) {
 
 
     material = new THREE.MeshBasicMaterial({ color: 0x1E90FF, wireframe: wires });
+    //material = new THREE.MeshLambertMaterial( { color:0xff0000} );
     addHolofoteCone(index, 0, 10, 0);
     addHolofoteArtic(index, 0, 14, 0);
 
@@ -173,12 +179,23 @@ function createHolofote(index, x, y, z) {
     index.position.z = z;
     grupo.add(index);
 }
-
+function onOroffLight(){
+  var state = !directional_light.visible;
+  directional_light.visible = state;
+}
 function createPainting(obj,x,y,z){
     'use strict';
 
     addSquares(obj);
 }
+
+function initMaterials(){
+    material_array = new Array(3);
+    material_array[0] = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: wires });
+    material_array[1] = new THREE.MeshLambertMaterial({ color: 0xFFFFFF});
+    material_array[2] = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, shininess: 30 });
+}
+
 
 
 function createScene() {
@@ -187,15 +204,21 @@ function createScene() {
     scene = new THREE.Scene();
 
     scene.add(new THREE.AxisHelper(100));
+    initMaterials();
 
     new Triangle(20,0,20);
     new Wall(0,0,0);
     new Holofote(-25, 25, 30);
-    new Holofote(-10, 25, 30);
+    holofote2 = new Holofote(-10, 25, 30);
     new Holofote(5, 25, 30);
     new Holofote(20, 25, 30);
-
+    directional_light = new THREE.DirectionalLight(0xffffff, 1);
+    directional_light.position.set(0, 20, 50);
+    directional_light.castShadow = true;
+    scene.add(directional_light);
     scene.add(grupo);
+    //directional_light.target = grupo.children[1];
+    //console.log(grupo.children[1]);
 
 }
 
@@ -216,7 +239,7 @@ function createCamera() {
 function createCamera2() {
   'use strict';
 
-  camera1[1] = new THREE.PerspectiveCamera( 45, 1920/1080, 1, 1000);
+  camera1[1] = new THREE.PerspectiveCamera( 65, 1920/1080, 1, 1000);
 
 
   camera1[1].position.x = 40;
@@ -264,17 +287,24 @@ function onKeyDown(e) {
         switch_camera(1);
         break;
     case 37://left arrow
-        selected_cannon.toggleLeftMovement();
         break
     case 39://right arrow
-        selected_cannon.toggleRightMovement();
+        if(material_counter < 2)
+          material_counter++;
+        else {
+          material_counter = 0;
+        }
+        console.log(material_counter);
+        for(var i = 0; i < grupo.children.length; i++){
+          for (var j = 0; j < grupo.children[i].children.length; j++){
+            grupo.children[i].children[j].material = material_array[material_counter];
+          }
+        }
         break
-
     case 69:  //E
-          right_cannon.toggleSelectedCannon();
           break;
     case 81: //Q
-          left_cannon.toggleSelectedCannon();
+          onOroffLight();
           break;
     case 82: //r
         if(Axistf){
